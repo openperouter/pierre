6a68957b2727039f1ef8b956ef9a513e14c66d95
Federico Paolinelli <fpaoline@redhat.com>
2025-01-28 17:09:05 +0100

    Add FRR configuration rendering and reloading logic.
    Adding a package with the FRR rendering logic and a way to invoke an
    endpoint after the new configuration is rendered.
    
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

---
+++ internal/frr/frr.go
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier:Apache-2.0
+
+package frr
+
+import (
+	"context"
+	"os"
+	"sync"
+
+	"github.com/go-kit/log"
+)
+
+type ConfigUpdater func(context.Context, string) error
+
+type FRR struct {
+	configFile string
+	logLevel   string
+	sync.Mutex
+}
+
+const ReloadSuccess = "success"
+
+// Create a variable for os.Hostname() in order to make it easy to mock out
+// in unit tests.
+var osHostname = os.Hostname
+
+func ApplyConfig(ctx context.Context, config *Config, updater ConfigUpdater) error {
+	hostname, err := osHostname()
+	if err != nil {
+		return err
+	}
+
+	config.Hostname = hostname
+	return generateAndReloadConfigFile(ctx, config, updater)
+}
+
+func NewFRR(logger log.Logger) *FRR {
+	res := &FRR{}
+	return res
+}
+
+/*
+func logLevelToFRR(level logging.Level) string {
+	// Allowed frr log levels are: emergencies, alerts, critical,
+	// 		errors, warnings, notifications, informational, or debugging
+	switch level {
+	case logging.LevelAll, logging.LevelDebug:
+		return "debugging"
+	case logging.LevelInfo:
+		return "informational"
+	case logging.LevelWarn:
+		return "warnings"
+	case logging.LevelError:
+		return "error"
+	case logging.LevelNone:
+		return "emergencies"
+	}
+
+	return "informational"
+}
+*/
